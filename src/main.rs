df78a9bc12514929d35e6a4790cf3b2aa9e24143
theNEOkun <sven2000@student.miun.se>
2022-05-25 13:56:06 +0200

    Working on making the board

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,40 @@ use snake::Snake;
 
 use std::io::{stdout, Write};
 
-fn main() {
-    let mut stdout = stdout();
-    //going into raw mode
-    enable_raw_mode().unwrap();
+const MAX_SIZE: usize = 16;
+
+fn print(o_str: &str, mut stdout: &std::io::Stdout) {
 
     //clearing the screen, going to top left corner and printing welcoming message
-    execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0, 0), Print(r#"ctrl + q to exit, ctrl + h to print "Hello world", alt + t to print "crossterm is cool""#))
+    execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0, 0), Print(o_str))
         .unwrap();
+}
+
+fn main() {
+    let mut stdout = stdout();
+
+    print(r#"q to exit"#, &stdout);
 
     let snake = Snake::new();
 
+    let mut board = vec![vec!["  "; MAX_SIZE]; MAX_SIZE];
+
+    let mut o_string = String::from("\n");
+
+    for (y_pos, each) in board.iter().enumerate() {
+        for (x_pos, mut string) in each.iter().enumerate() {
+            if x_pos == (MAX_SIZE - 1) || x_pos == 0 {
+                string = &&" #";
+            } else if y_pos == (MAX_SIZE-1) || y_pos == 0 {
+                string = &&" #";
+            }
+            o_string += string;
+        }
+        o_string += "\n";
+    }
+    print(&o_string, &stdout);
+
+    /*
     //key detection
     loop {
         //going to top left corner
@@ -33,6 +56,10 @@ fn main() {
         //matching the key
         match read().unwrap() {
             //i think this speaks for itself
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: KeyModifiers::NONE,
+            }) => break,
             Event::Key(KeyEvent {
                 code: KeyCode::Left,
                 modifiers: KeyModifiers::NONE,
@@ -53,7 +80,6 @@ fn main() {
             _ => (),
         }
     }
+    */
 
-    //disabling raw mode
-    disable_raw_mode().unwrap();
 }

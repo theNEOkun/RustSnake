d8c40163822c55037fe1c7ae37767c1cfe944678
theNEOkun <sven2000@student.miun.se>
2022-05-30 17:50:20 +0200

    Have an idea to use mpsc for reading keyboard

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,6 +1,8 @@
 pub mod helper_enums;
 
-use crossterm::event::{ Event, KeyEvent, KeyCode, KeyModifiers };
+use std::{sync::mpsc::*, thread, time::Duration};
+
+use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers};
 
 use self::helper_enums::{Directions, MoveOpt};
 
@@ -48,6 +50,10 @@ pub fn get_player_two(input: Event) -> MoveOpt<Directions> {
     }
 }
 
-pub fn handle_movement() {
+pub fn handle_movement(p1_rec: Receiver<Directions>, p2_rec: Receiver<Directions>) {
+    thread::spawn(|| if poll(Duration::from_millis(100)).unwrap() {
+        match read().unwrap() {
 
+        }
+    });
 }
